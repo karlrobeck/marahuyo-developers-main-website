9dca3e83a59780c6e07f09235acfd18b65aaed96
Karl Robeck <karlalferezfx@example.com>
2024-08-05 23:26:28 +0800

    landing page draft

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import Button from '$lib/components/ui/button/button.svelte';
 	import '../app.css';
 </script>
@@ -65,5 +65,23 @@
 	<div class="space-y-5 p-4 py-24">
 		<slot></slot>
 	</div>
-	<footer></footer>
+	<footer class="flex flex-col gap-5 border-t border-border py-12 pb-24">
+		<div class="flex flex-row items-center gap-5">
+			<img
+				class="h-12 w-12 rounded-md"
+				src="https://scontent.fmnl25-4.fna.fbcdn.net/v/t39.30808-6/454263195_122098053050459721_951208295823453167_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=pxeOlXmFuDYQ7kNvgFtDjnh&_nc_ht=scontent.fmnl25-4.fna&oh=00_AYAa3GKUaxM5HjNRoW8r54KoKuCGQKXUFVg1Er2QVfrIMQ&oe=66B5AC5D"
+				alt="Marahuyo developers Logo"
+			/>
+			<h4 class="font-bold">Marahuyo Developers</h4>
+		</div>
+		<span class="small">© 2024 Mayahuyo Developer Team</span>
+		<ul>
+			<li>
+				<Button variant="link" class="w-fit">Privacy Policy</Button>
+			</li>
+			<li>
+				<Button variant="link" class="w-fit">Terms of services</Button>
+			</li>
+		</ul>
+	</footer>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/button/button.svelte';
 	import * as Card from '$lib/components/ui/card/index.js';
-
+	import * as Accordion from '$lib/components/ui/accordion/index.js';
 	const programmingLanguages = [
 		{
 			name: 'Rust',
@@ -37,6 +37,13 @@
 				url: 'https://www.svgrepo.com/show/353622/c-sharp.svg',
 				alt: 'C Sharp Logo'
 			}
+		},
+		{
+			name: 'GO',
+			logo: {
+				url: 'https://www.svgrepo.com/show/452214/go.svg',
+				alt: 'Go Programming Language Logo'
+			}
 		}
 	];
 </script>
@@ -46,21 +53,58 @@
 		<span class="text-yellow-500">Transforming ideas</span> into <span class="">reality</span>
 	</h1>
 	<p class="lead">
-		It is a creative hub where imagination meets craftsmanship to transform ideas into tangible
-		realities. At Preline Agency, we specialize in turning conceptual visions into concrete forms,
-		whether it be through design, artistry, or technological innovation.
+		We are software developers that specialize on building fast, reliable safe programs for your
+		business.
 	</p>
 	<div class="flex flex-row items-center gap-5">
 		<Button>Contact Us</Button>
 		<Button variant="link">Learn More</Button>
 	</div>
 </section>
-<section>
+<section class="space-y-5 pb-12">
+	<h2 class="border-none">Our Services</h2>
+	<p class="lead pb-5">See our services that we offer for your needs.</p>
+	<div class="">
+		<Accordion.Root>
+			<Accordion.Item value="service-1">
+				<Accordion.Trigger>Github Project Consulation</Accordion.Trigger>
+				<Accordion.Content>
+					<p>We offer insdustry standard Code review</p>
+				</Accordion.Content>
+			</Accordion.Item>
+			<Accordion.Item value="service-2">
+				<Accordion.Trigger>Custom Website for your business</Accordion.Trigger>
+				<Accordion.Content>
+					<p>
+						We offer service to create website for your business, personal porfolio projects, System
+						Documentation, and many more.
+					</p>
+				</Accordion.Content>
+			</Accordion.Item>
+			<Accordion.Item value="service-3">
+				<Accordion.Trigger>Google Adsense Compilient website</Accordion.Trigger>
+				<Accordion.Content>
+					<p>
+						Out of the box, all website that we product comply with Google Adsense Specification for
+						easy approval
+					</p>
+				</Accordion.Content>
+			</Accordion.Item>
+			<Accordion.Item value="service-4">
+				<Accordion.Trigger>Figma Prototypes</Accordion.Trigger>
+				<Accordion.Content>
+					<p>We offer Figma Prototypes for your system / business</p>
+				</Accordion.Content>
+			</Accordion.Item>
+		</Accordion.Root>
+	</div>
+</section>
+<section class="space-y-5 pb-12">
 	<h2 class="border-none pb-0 font-bold">Your System, Your Choice!</h2>
 	<p class="lead pb-5">
 		We can build your system with programming languages that you're already familiar with.
 	</p>
-	<div class="grid grid-cols-5 items-center justify-between gap-5">
+	<div class="grid grid-cols-6 items-center justify-between gap-5">
 		{#each programmingLanguages as language}
 			<Card.Root>
 				<Card.Header class="flex flex-row items-center justify-center">
@@ -71,5 +115,14 @@
 				</Card.Footer>
 			</Card.Root>
 		{/each}
+	</div>
+</section>
+<section class="flex flex-col items-center space-y-5 pb-12">
+	<h1>Turn your ideas into reality</h1>
+	<p class="lead">Contact us for more information.</p>
+	<div>
+		<Button>Github</Button>
+		<Button>Whatsapp</Button>
+		<Button>Facebook</Button>
 	</div>
 </section>
